c622da5f0f6a8548d0f4757441e1528440ec0e3a
Maksim <mouxez@gmail.com>
2020-01-20 16:31:04 +0300

    fixes according to code review

--- source/js/script.js
+++ source/js/script.js
@@ -1,4 +1,4 @@
-var navToggle = document.querySelector(".main-nav__toggle");
+var navToggle = document.querySelector(".toggle");
 var navMain = document.querySelector(".main-nav");
 
 // navToggle.classList.remove("main-nav__toggle--nojs");
